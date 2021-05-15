500d46608c8f458adca75827459d8898c0b32f56
ericlearning <48952432+ericlearning@users.noreply.github.com>
2021-05-15 23:56:01 +0900

    README updated & run.py updated

--- run.py
+++ run.py
@@ -1,12 +1,18 @@
+from argparse import ArgumentParser
 from textbook_rsa import generate, encdec
 
-max_len = 128
+parser = ArgumentParser()
+parser.add_argument('-l', '--max-len', type=int, default=128, help='maximum message length')
+parser.add_argument('-m', '--message', type=str, default='HELLO WORLD', help='message to encrypt')
+args = parser.parse_args()
+
+max_len = args.max_len
 public_key, private_key = generate.generate_keys(max_len)
 
-message = "HELLO WORLD".upper()
+message = args.message.upper()
 encrypted_message = encdec.encrypt(public_key, message, max_len)
 decrypted_message = encdec.decrypt(encrypted_message, private_key, public_key)
 
-print(message)
-print(encrypted_message)
-print(decrypted_message)
+print(f'Message to encrypt: {message}')
+print(f'Encrypted message: {encrypted_message}')
+print(f'Decrypted message: {decrypted_message}')
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='ufo',
+    version='0.0.1',
+    description='A concise implementation of a textbook version RSA.',
+    url='https://github.com/ericlearning/Textbook-RSA',
+    author='ericlearning',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
